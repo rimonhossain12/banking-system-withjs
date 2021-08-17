a56596c686e195d6ae8de9a69e06e97b6fde6b4b
rimonspi12 <rimonmpi@gmail.com>
2021-08-17 11:50:05 +0600

    balance is added"

--- js/banking.js
+++ js/banking.js
@@ -4,18 +4,23 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     // console.log('deposit button is click');
     const depositInput = document.getElementById('deposit-input');
     const depositInputTotal = parseFloat(depositInput.value);
-
-
     // select depoist before value
-
     const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
-
-
     const depositTotalAmount = parseFloat(depositTotalText);
-    depositTotal.innerText = depositTotalAmount + depositInputTotal;
-
+    const totalDepositBalanceAmount = depositTotalAmount + depositInputTotal;
+    depositTotal.innerText = totalDepositBalanceAmount;
     depositInput.value = '';
+
+
+    // update current balance
+
+    const currentBalance = document.getElementById('balance-total');
+    const currentBalanceText = currentBalance.innerText;
+    const currentBalanceTotal = parseFloat(currentBalanceText);
+    // const currentBalanceText = parseFloat(currentBalance.innerText);
+
+    currentBalance.innerText = currentBalanceTotal + depositInputTotal;
 
 
 
