de447996daf4357c66c5595209989251c36c263f
rimonspi12 <rimonmpi@gmail.com>
2021-08-17 12:30:57 +0600

    withdraw working good

--- js/banking.js
+++ js/banking.js
@@ -1,16 +1,18 @@
-// select deposit button
-
+// handle deposit amount
 document.getElementById('deposit-button').addEventListener('click', function () {
+
     // console.log('deposit button is click');
     const depositInput = document.getElementById('deposit-input');
     const depositInputTotal = parseFloat(depositInput.value);
+
+
+
     // select depoist before value
     const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
     const depositTotalAmount = parseFloat(depositTotalText);
     const totalDepositBalanceAmount = depositTotalAmount + depositInputTotal;
     depositTotal.innerText = totalDepositBalanceAmount;
-    depositInput.value = '';
 
 
     // update current balance
@@ -24,4 +26,27 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
 
 
+    // clean the input value;
+    depositInput.value = '';
+});
+
+
+// handle in withdraw amount;
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawInputTotal = parseFloat(withdrawInput.value);
+    // console.log(withdrawInputTotal);
+
+    // withdraw amount totla
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalInput = withdrawTotal.innerText;
+    const withdrawTotalAmount = parseFloat(withdrawTotalInput);
+    // console.log(withdrawTotalAmount);
+
+    // update balance
+    withdrawTotal.innerText = withdrawInputTotal + withdrawTotalAmount;
+
+    // clean the input value
+    withdrawInput.value = '';
 })
