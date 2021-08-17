1abac3b9c740c3b0c2911a494065baa071f54dad
rimonspi12 <rimonmpi@gmail.com>
2021-08-17 11:29:55 +0600

    deposit is working well

--- js/banking.js
+++ js/banking.js
@@ -2,8 +2,8 @@
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     // console.log('deposit button is click');
-    const depositInput = document.getElementById('deposit-input').value;
-    const depositInputTotal = parseFloat(depositInput);
+    const depositInput = document.getElementById('deposit-input');
+    const depositInputTotal = parseFloat(depositInput.value);
 
 
     // select depoist before value
@@ -14,6 +14,8 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
     const depositTotalAmount = parseFloat(depositTotalText);
     depositTotal.innerText = depositTotalAmount + depositInputTotal;
+
+    depositInput.value = '';
 
 
 
